3393dca73c385abaa61be2ce85c88e87d558ba06
grantrisk <riskgrant@gmail.com>
2023-11-06 18:04:19 -0500

    Changes by cscrc

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'dart:math' as math;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:geolocator/geolocator.dart';
+
+import 'package:numenu/views/widgets/search_result.dart';
 
 void main() {
   runApp(const MyApp());
@@ -46,34 +51,39 @@ class _MyMapState extends State<MyMap> {
 
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      mapController: mapController,
-      options: MapOptions(
-        initialCenter: LatLng(34.2104, -77.8868),
-        initialZoom: 9.2,
-        onPositionChanged: (position, hasGesture) {
-          final zoom = position.zoom;
-          if (zoom != null) {
-            updateMarkerSize(zoom);
-          }
-        },
-      ),
-      children: [
-        TileLayer(
-          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          userAgentPackageName: 'com.example.app',
-        ),
-        MarkerLayer(
-          markers: [
-            Marker(
-              point: LatLng(34.2359, -77.9409),
-              width: markerSize,
-              height: markerSize,
-              child:
-                  Icon(Icons.location_on, color: Colors.red, size: markerSize),
+    return Stack(
+      children: <Widget>[
+        FlutterMap(
+          mapController: mapController,
+          options: MapOptions(
+            initialCenter: LatLng(34.2104, -77.8868),
+            initialZoom: 9.2,
+            onPositionChanged: (position, hasGesture) {
+              final zoom = position.zoom;
+              if (zoom != null) {
+                updateMarkerSize(zoom);
+              }
+            },
+          ),
+          children: [
+            TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+              userAgentPackageName: 'com.example.app',
+            ),
+            MarkerLayer(
+              markers: [
+                Marker(
+                  point: LatLng(34.2359, -77.9409),
+                  width: markerSize,
+                  height: markerSize,
+                  child: Icon(Icons.location_on,
+                      color: Colors.red, size: markerSize),
+                ),
+              ],
             ),
           ],
         ),
+        SearchResult(),
       ],
     );
   }
