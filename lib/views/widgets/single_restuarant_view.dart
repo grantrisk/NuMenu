3f8b636f612742fd5a2b9025f24934a33dbc95a2
crc6267 <cscrcapps@gmail.com>
2024-02-05 16:44:08 -0500

    single res view v1

--- lib/views/widgets/single_restuarant_view.dart
+++ lib/views/widgets/single_restuarant_view.dart
@@ -61,57 +61,140 @@ class _SingleRestaurantViewState extends State<SingleRestaurantView>
         .size
         .height;
 
-    var squareSize = screenWidth * 0.2; // Example: 20% of the screen width
-
-    var buttonPadding = EdgeInsets.fromLTRB(
-      screenWidth * 0.05,
-      screenHeight * 0.01,
-      screenWidth * 0.05,
-      screenHeight * 0.01,
-    );
+    var squareSize = screenWidth * 0.25; // Example: 20% of the screen width
 
     return Consumer<GlobalStateService>(
       builder: (context, state, child) {
-        return Column(
-          children: [
-            Container(
-              height: squareSize + screenHeight * 0.02,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                // TODO
-                /// This needs to be the length of the images list
-                /// This needs to be the length of the images list
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: buttonPadding,
-                    child: FadeTransition(
-                      opacity: _fadeControllers[index],
-                      // TODO
-                      /// Pass in the image as a param
-                      /// ```
-                      ///  RestaurantImage(image: imagesList[index])
-                      /// ```
-                      child: Container(
-                        width: squareSize,
-                        height: squareSize,
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.grey,
-                            width: 2.0,
+        return Padding(
+          padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.08),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                  height: squareSize + screenHeight * 0.02,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    // TODO
+                    /// This needs to be the length of the images list
+                    /// This needs to be the length of the images list
+                    itemCount: 10,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: determineButtonPadding(screenWidth, screenHeight, index),
+                        child: FadeTransition(
+                          opacity: _fadeControllers[index],
+                          // TODO
+                          /// Pass in the image as a param
+                          /// ```
+                          ///  RestaurantImage(image: imagesList[index])
+                          /// ```
+                          child: Container(
+                            width: squareSize,
+                            height: squareSize,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.grey,
+                                width: 2.0,
+                              ),
+                            ),
                           ),
                         ),
+                      );
+                    },
+                  ),
+                ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.width * 0.05,
+                ),
+                /// Restaurant type here
+                child: Text(
+                  resInfo['Name'],
+                  style: const TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.width * 0.05),
+              Row(
+                children: [
+                  Text(
+                    resInfo['Address'],
+                    style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.grey[400],
+                    ),
+                  ),
+                  const Spacer(flex:2),
+                  Padding(
+                    padding: EdgeInsets.only(right: MediaQuery.of(context).size.width * 0.08),
+                    child: Text(
+                      resInfo['Phone'],
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.grey[400],
                       ),
                     ),
-                  );
-                },
+                  ),
+                ],
               ),
-            ),
-
-          ],
+              SizedBox(height: MediaQuery.of(context).size.width * 0.05),
+              Row(
+                children: [
+                  const Text(
+                    '⭐',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                  SizedBox(width: MediaQuery.of(context).size.width * 0.02),
+                  Text(
+                    resInfo['Rating'].toString(),
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w800,
+                      color: Color.fromARGB(255, 251, 181, 29),
+                    ),
+                  ),
+                  SizedBox(width: MediaQuery.of(context).size.width * 0.02),
+                  Text(
+                    '(400) Google',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w800,
+                      color: Colors.grey[400]!,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         );
       },
     );
+  }
+
+  determineButtonPadding(screenWidth, screenHeight, index) {
+    if (index == 0) {
+      return EdgeInsets.fromLTRB(
+        screenWidth * 0.00,
+        screenHeight * 0.01,
+        screenWidth * 0.01,
+        screenHeight * 0.01,
+      );
+    } else {
+      return EdgeInsets.fromLTRB(
+        screenWidth * 0.02,
+        screenHeight * 0.01,
+        screenWidth * 0.01,
+        screenHeight * 0.01,
+      );
+    }
   }
 }
 
